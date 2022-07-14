af5dcc27cb4fbdee16594fea7abd792a8e354871
Fostertech <renato@fostertech.me>
2022-07-13 22:20:53 -0400

    fix some translations

--- js/translations.js
+++ js/translations.js
@@ -1,8 +1,11 @@
 $(function() {
 
     var t = {
+            "Águia Educação.": { 
+              en: "Aguia Education.",
+             },
             "Sua Edutech Tem Solução.": { 
-              en: "Solution for you Edutech.",
+              en: "Your Edutech has a solution.",
              },
             service_1 : { 
               en: "Projector with interactivity for classrooms and meetings",
@@ -141,15 +144,15 @@ $(function() {
               pt:"HOME",
             },
             barMenu_2:{
-              en:"WHO WE ARE",
+              en:"ABOUT",
               pt:"QUEM SOMOS",
             },
             barMenu_3:{
-              en:"CUSTOMERS",
+              en:"CLIENTS",
               pt:"CLIENTES",
             },
             barMenu_4:{
-              en:"PARTNERS",
+              en:"PARTNERSHIPS",
               pt:"PARCEIROS",
             },
             barMenu_5:{
@@ -161,11 +164,11 @@ $(function() {
               pt:"AUTOMAÇÃO",
             },
             barMenu_7:{
-              en:"LOCATION",
+              en:"RENTAL",
               pt:"LOCAÇÃO",
             },
             barMenu_8:{
-              en:"CONTACT US",
+              en:"CONTACT",
               pt:"FALE CONOSCO",
             },
             "Fale conosco": { 
